3a5cdff194a0af4f8b8cd1d1b793f3b6d9ba331a
Nemo <nemo.xun.jin.wang@gmail.com>
2021-12-09 15:41:24 -0600

    Removed user table and user model for redo.

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  before_action :authenticate_user!
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
-end
