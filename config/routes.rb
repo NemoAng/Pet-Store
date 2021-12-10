986c9d0fa8d577101661e9ed60b29b371da18b6f
Nemo <nemo.xun.jin.wang@gmail.com>
2021-12-09 20:09:29 -0600

    Fix user login/signup.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'#,
+    #omniauth_callbacks: 'omni_auth'
+  }
+  devise_scope :user do
+    get 'sign_in', to: 'users/sessions#new'
+    get 'sign_up', to: 'users/registrations#new'
+    get 'forgot_password', to: 'users/passwords#new'
+    get 'reset_password', to: 'users/passwords#edit'
+  end
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :categories
